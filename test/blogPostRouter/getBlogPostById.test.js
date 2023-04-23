f5bc8a7436be60c701210f2ec67533ad39814c69
cafe51 <cafecafe51@hotmail.com>
2023-04-23 04:45:55 -0400

    adicionado teste getBlogPostById.test.js

---
+++ test/blogPostRouter/getBlogPostById.test.js
@@ -0,0 +1,32 @@
+const chai = require('chai');
+const chaiHttp = require('chai-http');
+const sinon = require('sinon');
+const { sign } = require('../../src/utils/jwt');
+const { blog_post: blogPosts } = require('../../src/database/models');
+const { blogPostsMock } = require('../mocks');
+const app = require('../../src/app');
+
+const generateToken = (email) => sign(email);
+const token = generateToken('test@example.com');
+const { expect } = chai;
+chai.use(chaiHttp);
+
+describe('User API', () => {
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  it('retorna todos os posts', async () => {
+    const stub = sinon.stub(blogPosts, 'findAll');
+    stub.resolves(blogPostsMock[0]);
+
+    const httpResponse = await chai
+      .request(app)
+      .get('/post/1')
+      .set('Authorization', token);
+
+    expect(httpResponse).to.have.status(200);
+    expect(httpResponse.body).to.be.an('object');
+    expect(httpResponse.body).to.be.deep.equal(blogPostsMock[0]);
+  });
+});
