0a5ed9b129de8b3e1da4a5ca0dbb4fd725a2d4ac
cafe51 <cafecafe51@hotmail.com>
2023-04-24 03:12:28 -0400

    criado método que cria novo post no controller

--- src/controller/BlogPostController.js
+++ src/controller/BlogPostController.js
@@ -22,6 +22,16 @@ class BlogPostController {
       return res.status(500).json({ error: err.message });
     }
   };
+
+  createNewBlogPost = async (req, res) => {
+    try {
+      const { status, payload } = await this.service.createNewBlogPost(req.body);
+      if (status) return res.status(status).json({ message: payload });
+      return res.status(200).json(payload);
+    } catch (err) {
+      return res.status(500).json({ error: err.message });
+    }
+  };
 }
 
 module.exports = BlogPostController;
