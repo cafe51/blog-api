b7178ae6d8d5916e9824fdeb3b9cfd88091cc5dc
cafe51 <cafecafe51@hotmail.com>
2023-07-01 06:06:44 -0400

    feat: adicionado mensagem de boas vindas na primeira requisição

--- src/app.js
+++ src/app.js
@@ -5,7 +5,10 @@ const app = express();
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.status(200).json({ hello: 'world' });
+  res.status(200).json({
+    instruções: 'A api está pronta para uso! Você precisa estar logado, siga a documentação da api para conhecer os endpoints',
+    documentação: 'https://github.com/cafe51/blog-api#como-usar-a-api',
+  });
 });
 
 app.use(routes);
