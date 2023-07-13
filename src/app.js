bf84b28b30a64da93dafd4d59843bdbe511e9905
cafe51 <cafecafe51@hotmail.com>
2023-07-13 01:02:31 -0400

    fix: chore

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
-const cors = require('cors');
 const express = require('express');
 const routes = require('./routes/routes');
 
 const app = express();
-
-app.use(cors());
 app.use(express.json());
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'POST, GET, DELETE, OPTIONS, PUT');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 app.get('/', (req, res) => {
   res.status(200).json({
