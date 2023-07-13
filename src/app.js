226ae5ae83b93fea9412d33a14e828f002379274
cafe51 <cafecafe51@hotmail.com>
2023-07-13 00:56:27 -0400

    chore: cors

--- src/app.js
+++ src/app.js
@@ -1,7 +1,10 @@
+const cors = require('cors');
 const express = require('express');
 const routes = require('./routes/routes');
 
 const app = express();
+
+app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
