ca061bf958547d485440e5cf8182e0dd65e12989
cafe51 <cafecafe51@hotmail.com>
2023-07-13 02:45:25 -0400

    fix: cors

--- src/app.js
+++ src/app.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Methods', 'POST, GET, DELETE, OPTIONS, PUT');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   next();
 });
 
