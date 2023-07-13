41e5ac7089eb636ee7fb5e6bab630861506acc06
cafe51 <cafecafe51@hotmail.com>
2023-07-13 05:02:13 -0400

    fix: cors

--- src/app.js
+++ src/app.js
@@ -1,15 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes/routes');
 
 const app = express();
 app.use(express.json());
 
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Methods', 'POST, GET, DELETE, OPTIONS, PUT');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-  next();
-});
+app.use(cors());
 
 app.get('/', (req, res) => {
   res.status(200).json({
